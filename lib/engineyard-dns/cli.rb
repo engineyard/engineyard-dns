ea8b50d936c1e00f4896e5a33582a476ccb31de7
Martin Emde <martin.emde@gmail.com>
2011-05-24 12:49:36 -0700

    Clean up TextMate's trailing space

--- lib/engineyard-dns/cli.rb
+++ lib/engineyard-dns/cli.rb
@@ -25,7 +25,7 @@ module EngineYard
       method_option :override, :aliases    => ["-o"], :type => :boolean, :desc => "Override DNSimple records if they already exist"
       def assign(domain_name, name = "")
         say "Fetching AppCloud environment information..."; $stdout.flush
-        
+
         environment = fetch_environment(options[:environment], options[:account])
         account_name, env_name = environment.account.name, environment.name
         unless environment.instances.first
@@ -33,15 +33,15 @@ module EngineYard
         end
         public_hostname = environment.instances.first.public_hostname
         status          = environment.instances.first.status
-        
+
         # TODO - use DNS client to convert public_hostname into IP address
         unless public_hostname =~ /ec2-(\d+)-(\d+)-(\d+)-(\d+)/
           error "Cannot determine public IP from current hostname #{public_hostname}"
         end
         public_ip = "#{$1}.#{$2}.#{$3}.#{$4}"
 
-        say "Found AppCloud environment #{env_name} on account #{account_name} with IP #{public_ip}"        
-        
+        say "Found AppCloud environment #{env_name} on account #{account_name} with IP #{public_ip}"
+
         say ""
         say "Searching for #{domain_name} amongst your DNS providers..."; $stdout.flush
 
@@ -51,21 +51,21 @@ module EngineYard
         end
         say "Found #{domain_name} in #{provider_name} account"
         say ""; $stdout.flush
-        
+
         assign_dns(domain, account_name, env_name, public_ip, name, options[:override])
         assign_dns(domain, account_name, env_name, public_ip, "www", options[:override]) if name == ""
-          
+
         say "Complete!", :green
-        
+
         # ::DNSimple::Commands::ListRecords.new.execute([domain])
       end
-      
+
       desc "domains", "List available domains/zones from your DNS providers"
       def domains
         dns_provider_names.each do |provider_name|
           dns_provider = ::Fog::DNS.new({:provider => provider_name})
           domains      = dns_provider.zones
-          
+
           if domains.size == 0
             say "#{provider_name}: ", :yellow; say "none"
           else
@@ -77,7 +77,7 @@ module EngineYard
           end
         end
       end
-      
+
       desc "version", "show version information"
       def version
         require 'engineyard-dns/version'
@@ -100,7 +100,7 @@ module EngineYard
         shell.say "ERROR: #{text}", :red
         exit
       end
-      
+
       def watch_page_while(host, port, path)
         waiting = true
         while waiting
@@ -118,7 +118,7 @@ module EngineYard
           end
         end
       end
-      
+
       # Discover which DNS provider (DNSimple, etc) is controlling +domain_name+ (a zone)
       # and return [domain/zone, provider name]
       #
@@ -133,7 +133,7 @@ module EngineYard
         end
         [nil, nil]
       end
-      
+
       def assign_dns(domain, account_name, env_name, public_ip, name = "", override = false)
         if record = domain.records.select {|r| r.name == name}.first
           if override || ask_override_dns?(domain, name)
@@ -145,16 +145,16 @@ module EngineYard
         end
         say "Assigning "; say "#{domain_name domain, name} ", :green; say "--> "; say "#{public_ip} ", :green; say "(#{account_name}/#{env_name})"
         $stdout.flush
-        
+
         record = domain.records.create(:ip => public_ip, :name => name, :type => "A", :ttl => "60")
         say "Created A record for #{domain_name domain, name}"
       end
-      
+
       def ask_override_dns?(domain, name)
         ui = EY::CLI::UI::Prompter.backend
         ui.agree("Replace #{domain_name domain, name}: ", "y")
       end
-      
+
       # "myapp.com", "name" => "name.myapp.com"
       # "myapp.com", ""     => "myapp.com"
       def domain_name(domain, name = nil)
@@ -164,20 +164,20 @@ module EngineYard
           domain.domain
         end
       end
-      
+
       # Returns the list of DNS providers that the current user has access to
       # Includes the +fog_dns_providers+ list
       # TODO find credentials in alternate locations (e.g. ~/.dnsimple)
       def dns_provider_names
         fog_dns_provider_names
       end
-      
+
       # Returns the list of DNS providers that the current user has fog credentials
       # TODO how do I get the base list from fog?
       def fog_dns_provider_names
         ['AWS', 'Bluebox', 'DNSimple', 'Linode', 'Slicehost', 'Zerigo'] & Fog.providers
       end
-      
+
     end
   end
 end
